ab819bbe7cd541312b78461c93942f6f58e6ca5b
Jason Yao <jasonyao.jy@gmail.com>
2024-06-14 21:43:41 -0400

    Add feature intercept SIGINT and SIGTERM shutdown signals

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -45,6 +45,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
 	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
 	// flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 
@@ -73,6 +74,11 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+	}
+
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
 
 	srv := &http.Server{
