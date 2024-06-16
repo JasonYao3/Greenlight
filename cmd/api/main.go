229b38d2549facf1eea8ac46e510305e764a1837
Jason Yao <jasonyao.jy@gmail.com>
2024-06-16 17:38:38 -0400

    Add support to multiple dynamic origins

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -42,6 +43,10 @@ type config struct {
 		password string
 		sender   string
 	}
+
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -75,6 +80,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "<smtp-username>", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "<smtp-password>", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <smtp-email>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
